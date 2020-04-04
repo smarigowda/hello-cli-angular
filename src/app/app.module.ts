cdc0ca7a310eb6279283215cd56cc6d88aee0a43
Santosh Marigowda <santosharakere@gmail.com>
2020-04-04 11:13:51 +0100

    created a product service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
+import { ProductService } from './shared/product.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, ProductComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [ProductService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
