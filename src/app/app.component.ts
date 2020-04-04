340cf5ae4040fd9ad38638c4e0cfa9d8ed474500
Santosh Marigowda <santosharakere@gmail.com>
2020-04-04 07:56:16 +0100

    binding update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'hello-cli';
+  title = 'hello-cli welcome to Angular, A Modular SPA Framework !';
 }
